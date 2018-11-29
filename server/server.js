f5d294ae3898f1bcbdea9f7a43a23aa05eadf0d7
fernandotgo <fernandotgo@gmail.com>
2018-11-29 14:16:36 +0000

    First deploy

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://mongo:canelo23@ds039404.mlab.com:39404/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
