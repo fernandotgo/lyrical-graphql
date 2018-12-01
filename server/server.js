6ff6344e8a3616943b9dcfe684481918793ae4cb
fernandotgo <fernandotgo@gmail.com>
2018-12-01 01:16:58 +0000

    Song list

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = 'mongodb://jajaja:jojojo@noserverhere.com:39404/lyricaldb';
+const MONGO_URI = '';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
