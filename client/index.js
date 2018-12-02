8c9d31ca73e67b7eb995db5c559be1c8914cc44f
Fernando <fernandotgo@gmail.com>
2018-12-02 12:01:46 -0600

    Adding React router and song list

--- client/index.js
+++ client/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+
+import App from './components/SongList';
 
 import SongList from './components/SongList';
 
@@ -10,7 +13,11 @@ const client = new ApolloClient({});
 const Root = () => {
   return ( 
       <ApolloProvider client={client}>
-       <SongList /> 
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={SongList} />
+        </Route>
+      </Router>
       </ApolloProvider>
   );
 };
