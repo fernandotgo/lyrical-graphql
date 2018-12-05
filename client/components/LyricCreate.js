eeb7bb676beeeb65a5b2ed0ffb3de73337d937c9
fernandotgo <fernandotgo@gmail.com>
2018-12-05 14:17:31 +0000

    Add lyric text

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,27 @@
+import React , {Component} from 'react';
+
+class LyricCreate extends Component {
+    constructor(props){
+        super(props);
+        this.state = { content: ''};
+    }
+
+    onSubmit(event)
+    {
+        event.preventDefault();
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.onSubmit.bind(this)}>
+            <label>Add a lyric</label>
+            <input 
+            value={this.state.content}
+            onChange={event => this.setState({content: event.target.value})}
+            />
+            </form>
+        );
+    }
+}
+
+export default LyricCreate;
