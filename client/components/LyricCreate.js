6286591c58098ef5c9b9d00bea0f1afb2dfdc3e5
Fernando Caceres <fernandotgo@gmail.com>
2018-12-05 21:33:55 -0600

    Lyric list

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,4 +1,6 @@
 import React , {Component} from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricCreate extends Component {
     constructor(props){
@@ -9,6 +11,13 @@ class LyricCreate extends Component {
     onSubmit(event)
     {
         event.preventDefault();
+
+        this.props.mutate({
+            variables:{
+                content: this.state.content,
+                songId: this.props.songId
+            }
+        }).then(() => this.setState({content: ''}));
     }
 
     render(){
@@ -24,4 +33,16 @@ class LyricCreate extends Component {
     }
 }
 
-export default LyricCreate;
+const mutation = gql`
+mutation AddLyricToSong($content: String, $songId:ID){
+    addLyricToSong(content: $content, songId: $songId){
+      id
+      title
+      lyrics{
+        content
+      }
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricCreate);
