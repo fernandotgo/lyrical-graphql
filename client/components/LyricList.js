65a1ca2eede72ea2072b0843b630f10889299318
Fernando Caceres <fernandotgo@gmail.com>
2018-12-09 11:38:45 -0600

    Working likes with optimistic response

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,20 +1,36 @@
 import React, {Component} from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { optimistic } from 'apollo-client/optimistic-data/store';
 
 class LyricList extends Component{
 
-    onLike(id){
-        console.log(id);
+    onLike(id,likes){
+        this.props.mutate({ 
+            variables: {id},
+            optimisticResponse:{
+                __typename: 'Mutation',
+                likeLyric:{
+                    id: id,
+                    __typename: 'LyricType',
+                    likes: likes + 1
+                }
+            }
+        })
     }
 
     renderLyrics(){
-        return this.props.lyrics.map( ({id, content}) => {
+        return this.props.lyrics.map( ({id, content,likes}) => {
             return (
             <li key={id} className="collection-item">
                 {content}
-                <i 
-                   className="material-icons"
-                   onClick={() => this.onLike(id)}
-                >thump_up</i>
+                <div className="vote-box">
+                    <i 
+                    className="material-icons"
+                    onClick={() => this.onLike(id,likes)}
+                    >thump_up</i>
+                    {likes}
+                </div>
             </li>
             );
         });
@@ -29,4 +45,13 @@ class LyricList extends Component{
     }
 }
 
-export default LyricList;
+const mutation = gql`
+mutation LikeLyrig($id: ID!){
+    likeLyric(id: $id){
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
